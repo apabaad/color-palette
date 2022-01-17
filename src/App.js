d83901383f12773b8296bc7437876351eb15b4b0
apabaad <apabaad@yahoo.com>
2022-01-17 17:38:31 +1100

    color palette completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
+const range = 256;
 function App() {
+  const [breakPoints, setBreakPoints] = useState([]);
+  const [colorClicked, setColorClicked] = useState();
+
+  useEffect(() => {
+    let arr = [];
+    for (let i = 1; i <= range; i++) {
+      if (i % 8 === 0) {
+        arr[i] = i;
+      }
+    }
+    setBreakPoints(arr);
+  }, []);
+
+  console.log(breakPoints);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Programming challenge sent by EHG Developers! </h2>
+      <div className="container">
+        <div className="color-palette">
+          {breakPoints.map((r) => {
+            return breakPoints.map((g) => {
+              return breakPoints.map((b) => {
+                return (
+                  <span
+                    style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}
+                    onClick={() => setColorClicked(`${r},${g},${b}`)}
+                    key={`${r},${g},${b}`}
+                  ></span>
+                );
+              });
+            });
+          })}
+        </div>
+        <h1 style={{ color: `rgb(${colorClicked})` }}>
+          {' '}
+          You clicked the color RGB({colorClicked})
+        </h1>
+      </div>
     </div>
   );
 }
